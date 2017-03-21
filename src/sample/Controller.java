41fd1f8e7ed0aaba53a0435aeacedb406fae28ac
Huy <hqn103@yahoo.com>
2017-03-21 01:28:06 -0400

    almost working

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,32 +1,68 @@
 package sample;
 
-import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
-import javafx.scene.control.Toggle;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
 
-public class Controller {
-    private String[] questions = {
-            "Which group of American Indians used bison as a resource?",
-            "Which American Indians traveled by walking and using canoes?",
-            "Which group used adobe to build their homes?"};
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML private Label actiontarget;
-    @FXML private Label lbl_question;
-    @FXML private ToggleGroup toggleGroupAnswer;
+public class Controller implements Initializable {
+    @FXML private Label txtQuestion;
+    @FXML private RadioButton choiceA;
+    @FXML private RadioButton choiceB;
+    @FXML private RadioButton choiceC;
+    @FXML private Label displayResult;
 
-    @FXML protected void handleNextButtonAction(MouseEvent mouseEvent) {
-        lbl_question.setText(questions[1]);
-        actiontarget.setText(questions[1]);
+    @FXML private StackPane lamanhtest;
+
+    String [] questions = {"Question 1", "Question 2", "Question 3"};
+    String [][] answers = { {"1.a", "1.b", "1.c"}, {"2.a", "2.b", "2.c"}, {"3.a", "3.b", "3.c"} };
+    String [] correctAnswer = { "1.b", "2.a", "3.a" };
+    int questionNumber = 0;
+
+    @Override public void initialize(URL url, ResourceBundle rb) {
+        /**Button btnNext = new Button();
+        btnNext.setText("Click Me");
+        lamanhtest.getChildren().add(btnNext);
+         **/
+        loadTestQuestion(questionNumber);
     }
 
-    @FXML protected void handleAnswerSelectedAction(MouseEvent mouseEvent) {
-        RadioButton clickButton = (RadioButton) mouseEvent.getSource();
-        actiontarget.setText(clickButton.getId());
+    @FXML protected void nextQuestion(MouseEvent mouseEvent) {
+        if (questionNumber < (questions.length - 1)) {
+            questionNumber++;
+            loadTestQuestion(questionNumber);
+        }
+    }
+
+    @FXML protected void previousQuestion(MouseEvent mouseEvent) {
+        if (questionNumber > 0) {
+            questionNumber--;
+            loadTestQuestion(questionNumber);
+        }
+    }
+
+    @FXML protected void selectAnswer(MouseEvent mouseEvent) {
+        RadioButton selected = (RadioButton) mouseEvent.getSource();
+        if (selected.getText() == correctAnswer[questionNumber]) {
+            displayResult.setText("you choose " + selected.getText() + " is correct");
+        } else {
+            displayResult.setText(selected.getText() + " is wrong");
+        }
+
+
     }
 
 
+    private void loadTestQuestion(int pQuestionNumber) {
+            txtQuestion.setText(questions[pQuestionNumber]);
+            choiceA.setText(answers[pQuestionNumber][0]);
+            choiceB.setText(answers[pQuestionNumber][1]);
+            choiceC.setText(answers[pQuestionNumber][2]);
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,13 +15,25 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Pop Quiz Time");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        //Parent root = FXMLLoader.load(getClass().getResource("lamanh.fxml"));
+
+        //StackPane root = new StackPane();
+        //loadTest(root);
+
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 949, 460));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private void loadTest(StackPane pStackPane) {
+        Button btnNext = new Button("Next");
+        Label lblQuestion = new Label("Question number 1");
+        pStackPane.getChildren().add(lblQuestion);
+        pStackPane.getChildren().add(btnNext);
     }
 }
